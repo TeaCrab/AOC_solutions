fbee81812238cd3d72e33d9f28772ff5a32afeed
Brent Liu <TeaCrab@users.noreply.github.com>
2024-05-11 23:24:25 -0400

    Update aoc2023day1.py
    Improved solution for overlapping words of digit

--- aoc2023day1.py
+++ aoc2023day1.py
@@ -15,8 +15,12 @@ def ore(*args):
 def cgroup(term, groupname=''):
     return '('+term+')' if not groupname else f"(?P<{groupname}>{term})"
 
-double = r"(?:(?<=\n).*?____.*____.*?\n)"
-single = r"(?:(?<=\n).*?____.*?\n)"
+# Solves the text overlap issue
+def lookahead(term):
+    return r"(?="+term+")"
+
+# double = r"(?:(?<=\n).*?____.*____.*?\n)"
+# single = r"(?:(?<=\n).*?____.*?\n)"
 digits = {
     "1"     : "1",
     "2"     : "2",
@@ -38,17 +42,16 @@ digits = {
     "nine"  : "9",
 }
 
-digit = cgroup(oretup(tuple(e for e in digits.keys() if len(e) > 1) + (r"\d", )))
+# This solution doesn't do well when strictly two and only 2 words of digits are overlapping
+# part1 = ore(double.replace('____', r"(\d)"), single.replace('____', r"(\d)"))
+# part2 = ore(double.replace('____', digit), single.replace('____', digit))
 
-part1 = ore(double.replace('____', r"(\d)"), single.replace('____', r"(\d)"))
-part2 = ore(double.replace('____', digit), single.replace('____', digit))
+part1 = r"(\d)"
+part2 = lookahead(cgroup(oretup(tuple(e for e in digits.keys() if len(e) > 1) + (r"\d", ))))
 
 def answer(part):
     RE = re.compile(part, re.IGNORECASE)
-    data = RE.findall(INPUT)
-    data = [[e for e in tup if e!=''] for tup in data]
-    data = [e * int(2 // len(e)) for e in data]
-    func = lambda tup: int(''.join([digits[e] for e in tup]))
-    return sum([func(tup) for tup in data])
+    data = [RE.findall(e) for e in re.split(r"\n+", INPUT)]
+    return sum([int(''.join((digits[e[0]], digits[e[-1]]))) for e in data if e])
 
 code.interact(local=locals())
